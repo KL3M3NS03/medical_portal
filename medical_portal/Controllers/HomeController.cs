b204b88dff712dd30c2e6fc3b6710bd1c0c5bb6a
KL3M3NS03 <65786668+KL3M3NS03@users.noreply.github.com>
2022-02-07 08:12:16 +0100

    Dodaj pliki projektów.

---
+++ medical_portal/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using medical_portal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace medical_portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult FreeVisits()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
